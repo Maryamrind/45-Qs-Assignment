1e1aa356f51093db4cca5f1f37e7499bb490cd4d
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:59:28 -0700

    Add files via upload

---
+++ Step 09/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Assigment # 9
+let favoritedResult = 6;
+let messageResult = `My favourite number is ${favoritedResult}`;
+console.log(messageResult);
---
+++ Step 09/app.ts
@@ -0,0 +1,12 @@
+// Assignment # 9
+/**Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message 
+ * that reveals your favorite number. Print that message.*/
+
+//Storing a favorite number in a variable
+let favoritedResult:number = 6;
+
+//Creating a message that reveals your favorite number
+let messageResult:string = `My favourite number is ${favoritedResult}`
+
+//Printing the message
+console.log(messageResult)
