4794efcbb499a0199d38e5eb086978f049765ef3
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:36:47 -0700

    Add files via upload

---
+++ Step 26/app.js
@@ -0,0 +1,20 @@
+"use strict";
+// Assignment # 26
+/*If the alien's color is green,  print a statement that
+the player just earned 5 points for shooting the alien.*/
+let allien_color = "Green";
+if (allien_color === "Green") {
+    console.log("The player just earned 5 points for shooting the allien");
+}
+else {
+    console.log("The player just earned 10 points for shooting the allien");
+}
+/*If the alien's color isn't green,  print a statement that
+the player just earned 10 points for shooting the alien.*/
+let allien_color1 = "Yellow";
+if (allien_color1 === "Green") {
+    console.log("The player just earned 5 points for shooting the allien");
+}
+else {
+    console.log("The player just earned 10 points for shooting the allien");
+}
---
+++ Step 26/app.ts
@@ -0,0 +1,39 @@
+// Assignment # 26
+
+/**Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+• Write one version of this program that runs the if block and another that runs the else block */
+
+
+/*If the alien's color is green,  print a statement that 
+the player just earned 5 points for shooting the alien.*/
+
+let allien_color = "Green";
+
+if (allien_color ==="Green"){
+    console.log("The player just earned 5 points for shooting the allien");
+    
+}
+
+else{
+    console.log("The player just earned 10 points for shooting the allien");
+    
+}
+
+/*If the alien's color isn't green,  print a statement that 
+the player just earned 10 points for shooting the alien.*/
+
+let allien_color1 = "Yellow";
+
+if (allien_color1 ==="Green"){
+    console.log("The player just earned 5 points for shooting the allien");
+    
+}
+
+else{
+    console.log("The player just earned 10 points for shooting the allien");
+    
+}
