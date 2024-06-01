12f94df0e6d65825fa98cb485aa1c7fdd7ea7990
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:39:23 -0700

    Add files via upload

---
+++ Step 27/app.js
@@ -0,0 +1,44 @@
+"use strict";
+// Assignment # 27
+// If the allien is green,print a message that player earned 5 points.
+let allien_color = "Green";
+if (allien_color === "Green") {
+    console.log("The player earned 5 points");
+}
+else if (allien_color === "yellow") {
+    console.log("The player earned 10 points");
+}
+else if (allien_color === "Red") {
+    console.log("The player earned 15 points");
+}
+else {
+    console.log("unknown color");
+}
+// If the allien color is Yellow,print a message that player earned 10 points.
+let allien_color1 = "Yellow";
+if (allien_color1 === "Green") {
+    console.log("The player earned 5 points");
+}
+else if (allien_color1 === "Yellow") {
+    console.log("The player earned 10 points");
+}
+else if (allien_color1 === "Red") {
+    console.log("The player earned 15 points");
+}
+else {
+    console.log("unknown color");
+}
+// If the allien is red,print a message that player earned 15 points.
+let allien_color2 = "Red";
+if (allien_color2 === "Green") {
+    console.log("The player earned 5 points");
+}
+else if (allien_color2 === "yellow") {
+    console.log("The player earned 10 points");
+}
+else if (allien_color2 === "Red") {
+    console.log("The player earned 15 points");
+}
+else {
+    console.log("unknown color");
+}
---
+++ Step 27/app.ts
@@ -0,0 +1,79 @@
+// Assignment # 27
+
+/**Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+
+• If the alien is yellow, print a message that the player earned 10 points.
+
+• If the alien is red, print a message that the player earned 15 points.
+
+• Write three versions of this program, making sure each message is printed for the appropriate color alien. */
+
+// If the allien is green,print a message that player earned 5 points.
+
+let allien_color = "Green"
+
+if(allien_color==="Green"){
+    console.log("The player earned 5 points");
+
+}
+else if(allien_color==="yellow"){
+    console.log("The player earned 10 points");
+
+}
+else if(allien_color==="Red"){
+    console.log("The player earned 15 points");
+
+}
+else{
+    console.log("unknown color");
+    
+}
+    
+
+// If the allien color is Yellow,print a message that player earned 10 points.
+
+
+let allien_color1 = "Yellow"
+
+if(allien_color1==="Green"){
+    console.log("The player earned 5 points");
+
+}
+else if(allien_color1==="Yellow"){
+    console.log("The player earned 10 points");
+
+}
+else if(allien_color1==="Red"){
+    console.log("The player earned 15 points");
+
+}
+else{
+    console.log("unknown color");
+    
+}
+
+// If the allien is red,print a message that player earned 15 points.
+
+
+let allien_color2 = "Red"
+
+if(allien_color2==="Green"){
+    console.log("The player earned 5 points");
+
+}
+else if(allien_color2==="yellow"){
+    console.log("The player earned 10 points");
+
+}
+else if(allien_color2==="Red"){
+    console.log("The player earned 15 points");
+
+}
+else{
+    console.log("unknown color");
+    
+}
+
+
+
