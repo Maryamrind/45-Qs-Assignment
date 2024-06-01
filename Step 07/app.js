5e3b39ef676680ac5cfeac866717f1f238687915
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:46:18 -0700

    Add files via upload

---
+++ Step 07/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assigment # 7
+let addition = 4 + 4;
+console.log("Addition of 4 to 4 is:", addition);
+let subtraction = 50 - 42;
+console.log("Subtraction of 42 from 50 is:", subtraction);
+let multiplication = 2 * 4;
+console.log("Multiplication of 2 by 4 is:", multiplication);
+let division = 40 / 5;
+console.log("Division of 40 by 5 is:", division);
---
+++ Step 07/app.ts
@@ -0,0 +1,21 @@
+//Assignment # 7
+
+/**Number Eight: Write addition, subtraction, multiplication, and division operations 
+ * that each result in the number 8. 
+ * Be sure to enclose your operations in print statements to see the results. */
+
+let addition:number = 4+4
+
+console.log("Addition of 4 to 4 is:", addition)
+
+let subtraction:number = 50-42
+
+console.log("Subtraction of 42 from 50 is:", subtraction)
+
+let multiplication:number = 2*4
+
+console.log("Multiplication of 2 by 4 is:",multiplication)
+
+let division :number = 40/5
+
+console.log("Division of 40 by 5 is:",division)
