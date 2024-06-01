ba883bf1829fdecf05e34d3134924c832ee28dc8
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:37:44 -0700

    Add files via upload

---
+++ Step 05/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assigment # 05
+//assigment # 4
+let author = "Imran Khan";
+let famQuote = "sab se pehly ap ne ghbrana nahi hy";
+console.log(`${author} once said,"${famQuote}"`);
+//assigment # 5
+let famPerson = "Imran Khan";
+let message = `${author} once said,"${famQuote}"`;
+console.log(message);
---
+++ Step 05/app.ts
@@ -0,0 +1,17 @@
+//Assignment # 05
+
+/**Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+ * Then compose your message and store it in a new variable called message. Print your message. */
+    
+//assigment # 4
+let author:string = "Imran Khan";
+let famQuote:string = "You lose when you give up";
+
+console.log(`${author} once said,"${famQuote}"`);
+
+
+//assigment # 5
+let famPerson:string ="Imran Khan";
+let message:string = `${author} once said,"${famQuote}"`;
+
+console.log(message)
