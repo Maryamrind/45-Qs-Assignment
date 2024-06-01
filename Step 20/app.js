5985276d854e249cb150e5a5721173d363b652ee
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:01:02 -0700

    Add files via upload

---
+++ Step 20/app.js
@@ -0,0 +1,6 @@
+// Assignment # 20
+var countiesName = ["Pakistan", "Saudi Arabia", "UAE", "Japan", "Turkey"];
+//print list of countries
+console.log("List of countries:", countiesName.forEach(function (country) {
+    console.log(country);
+}));
---
+++ Step 20/app.ts
@@ -0,0 +1,14 @@
+// Assignment # 20
+
+/**Think of something you could store in a array. For example, you could make a list of
+ *  mountains, rivers, countries, cities, languages, or anything else you’d like. 
+ * Write a program that creates a list containing these items. */
+
+let countiesName:string[] = ["Pakistan","Saudi Arabia","UAE","Japan","Turkey"];
+//print list of countries
+
+console.log("List of countries:",countiesName.forEach(country => {
+    console.log(country);
+    
+    
+}));
