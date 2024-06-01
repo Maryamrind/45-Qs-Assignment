ac949f375261f5a9d3ba4353bba0d4944485f2cf
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 11:55:05 -0700

    Add files via upload

---
+++ Step 19/app.js
@@ -0,0 +1,24 @@
+"use strict";
+//  Assignment # 19
+//working with one of the programs from ex # 14 - 18.
+// printing the message indicating the number of people you are inviting to dinner 
+let guestList = ["Mehak Faizan", "Laraib Owais", "Muntaha Ibrahim"];
+console.log("unfortunately! the new dinner table wont arrive so, we can't invite you.");
+guestList.unshift("Faizan Qureshi", "Fizza Qureshi");
+// print message updated list
+console.log("updated list of guest", guestList);
+// remove guest from the list
+while (guestList.length > 2) {
+    let removedGuest = guestList.pop();
+    if (removedGuest !== undefined) {
+        console.log(`Sorry, ${removedGuest}, we can't invite you.`);
+    }
+}
+// }print a amessage to each of the two people still on your list letting them know
+guestList.forEach(guest => {
+    console.log(`Dear ${guest},you both are invited for the dinner.`);
+});
+// remove 2 more names from the list
+guestList.splice(0, guestList.length);
+// print updated empty list
+console.log("updated list of guest:", guestList);
