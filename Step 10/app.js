86b6ab7355381e7e8199230f979d844190354445
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 11:04:08 -0700

    Add files via upload

---
+++ Step 10/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Assigment # 10
+// this program add two numbers and print the result
+// define two numbers
+let number1 = 10;
+let number2 = 20;
+// Add two numbers
+let result = number1 + number2;
+// print result
+console.log(result);
---
+++ Step 10/app.ts
@@ -0,0 +1,15 @@
+// Assignment # 10
+/**Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+ * If you don’t have anything specific to write because your programs are too simple at this point,
+ *  just add your name and the current date at the top of each program file. 
+ * Then write one sentence describing what the program does */
+
+// this program add two numbers and print the result
+// define two numbers
+let number1:number = 10;
+let number2:number = 20;
+// Add two numbers
+let result:number = number1+number2
+// print result
+console.log(result);
+
