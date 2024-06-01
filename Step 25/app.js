4e465c7cd1f5ed944bd4b6fb7cbaa24c5252fb38
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:31:42 -0700

    Add files via upload

---
+++ Step 25/app.js
@@ -0,0 +1,12 @@
+"use strict";
+//Assignment # 25
+/*Write an if statement to test wether the allien color  is green .*/
+let allien_color = "Green";
+if (allien_color === "Green") {
+    console.log("player just earned 5 points.");
+}
+//Passed to find green color
+allien_color = "Yellow";
+if (allien_color === "Green") {
+    console.log("player just earned 5 points.");
+} //failed to find greencolor 
