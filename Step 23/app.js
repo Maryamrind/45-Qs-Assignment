2dd45fb74564e0ddab4a5b9564211d740b0fd99d
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:19:23 -0700

    Add files via upload

---
+++ Step 23/app.js
@@ -0,0 +1,42 @@
+"use strict";
+// Assignment # 23
+// conditional test.Write series of conditional tests
+// Test # 1
+let car = `subaru`;
+console.log("Is car ==`subaru`? I predict True.");
+console.log(car == `subaru`); //True
+// Test # 2
+console.log("Is car ==`Toyota`? I predict False.");
+console.log(car == ` toyota`); //False
+// Test # 3
+let number = 10;
+console.log("Is number > 10? I predict false");
+console.log(number > 10); //False
+//Test # 4
+let x = 9543;
+console.log("Is x !== 7568? I predict True");
+console.log(x !== 7568); //True
+//Test # 5
+let Speed = `Velocity`;
+console.log("Is Speed!=Velocit? I predict False.");
+console.log(Speed != `Velocity`); //False
+//Test # 6
+let grade = `B`;
+console.log("Is grade === B? I predict True.");
+console.log(grade === `B`); //True
+//Test # 7 
+let age = 24;
+console.log("Is age < 35 || age != 25 ? I predict False.");
+console.log(age > 35 || age != 24); // False
+//Test # 8
+let marks = 765;
+console.log("Is marks >= 750 && marks < 760? I predict True");
+console.log(marks >= 750 && marks < 770); //True
+// Test # 9
+let a = 10;
+console.log("Is (a>0) || (a<=8)? I predict True.");
+console.log((a > 0) || (a <= 8)); //True
+//Test # 10
+let b = 980;
+console.log("Is 980 <= 975? I predict False");
+console.log(980 <= 975);
---
+++ Step 23/app.ts
@@ -0,0 +1,78 @@
+// Assignment # 23
+
+/**Conditional Tests: Write a series of conditional tests.
+ *  Print a statement describing each test and your prediction for the results of each test. 
+ * • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False*/
+
+// conditional test.Write series of conditional tests
+
+
+// Test # 1
+let car =`subaru`
+console.log("Is car ==`subaru`? I predict True.");
+console.log(car==`subaru`);//True
+
+// Test # 2
+
+console.log("Is car ==`Toyota`? I predict False.");
+console.log(car==` toyota`); //False
+
+// Test # 3
+let number :number = 10
+console.log("Is number > 10? I predict false");
+console.log(number>10); //False
+
+//Test # 4
+
+let x = 9543
+
+console.log("Is x !== 7568? I predict True" );
+console.log(x!== 7568); //True
+
+//Test # 5
+let Speed = `Velocity`
+console.log("Is Speed!=Velocit? I predict False.");
+console.log(Speed!=`Velocity`);//False
+
+//Test # 6
+
+let grade = `B`
+console.log("Is grade === B? I predict True.");
+console.log(grade ===`B`);  //True
+
+//Test # 7 
+let age = 24
+console.log("Is age < 35 || age != 25 ? I predict False.");
+console.log( age > 35 || age != 24 ); // False
+
+//Test # 8
+let marks = 765
+console.log("Is marks >= 750 && marks < 760? I predict True");
+ console.log(marks >= 750 && marks < 770); //True
+
+ // Test # 9
+
+ let a = 10
+ console.log("Is (a>0) || (a<=8)? I predict True.");
+ console.log((a>0) || (a<=8));//True
+ 
+ //Test # 10
+
+ let b = 980
+ console.log("Is 980 <= 975? I predict False");
+ console.log(980<= 975);
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
