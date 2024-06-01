808d33cf4632a5aa4cc6ea1f25e2d66da526a676
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:42:14 -0700

    Add files via upload

---
+++ Step 06/app.js
@@ -0,0 +1,6 @@
+// Assignment #06
+//stripping name with white space
+var nameWithWhiteSpace = "\t \n Maryam Rind\t \n";
+console.log("name with wide space:", nameWithWhiteSpace);
+var strippedName = nameWithWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
