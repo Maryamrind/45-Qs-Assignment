4a5c22dbdcc89753db276a1fc8835156a1920936
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:34:39 -0700

    Add files via upload

---
+++ Step 04/app.js
@@ -0,0 +1,8 @@
+//Assignment # 4
+/**Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+ * Your output should look something like the following, including the quotation marks:
+ * Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+var famQuote = "you lose when you giveup";
+var author = "Imran Khan";
+// Print the quote and the name of its author.
+console.log("".concat(author, " once said; \"").concat(famQuote, ".\""));
---
+++ Step 04/app.ts
@@ -0,0 +1,13 @@
+//Assignment # 4
+/**Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+ * Your output should look something like the following, including the quotation marks:
+ * Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+
+
+let famQuote ="you lose when you giveup";
+let author = "Imran Khan";
+
+// Print the quote and the name of its author.
+console.log(`${author} once said; "${famQuote}."`)
+
+
