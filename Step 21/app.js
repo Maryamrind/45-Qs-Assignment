2e48d1da62c094b65e24f27755109973fbccaf0c
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:06:47 -0700

    Add files via upload

---
+++ Step 21/app.js
@@ -0,0 +1,74 @@
+"use strict";
+// // //Assignment # 21
+// let student = {
+//     name:"Abdullah",
+//     rollNumber: 2345,
+//     grades: [85,90,75]
+// };
+// console.log(student.grades[1]);
+// let product= {
+//     name: "laptop",
+//     price:"40000",
+//     description:"high performance laptop with fast processer"
+// }
+// console.log(product.price);
+// //type interface
+// interface Person{
+//     name:string;
+//     age:number;
+//     city:string;
+// }
+// let person1:Person={
+//     name:"Abdullah",
+//     age: 20,
+//     city:"Karachi",
+// }
+// let person2:Person={
+//     name: "A.rehman",
+//     age: 25,
+//     city: "Pishawar",
+// }
+// console.log(person2.name);
+// // //type annotation
+// // let myVariabl: string = "hello";
+// // console.log(myVariabl);
+// // //type infers
+// let anotherVariable = `world`
+// console.log(anotherVariable);
+//initial object
+// let person ={
+//     name: "Ali",
+//     age: 27,
+//     city: "Islamabad",
+// }
+// //updated property
+// person.age = 35;
+// person.city ="Queta"
+// // console.log(person);
+// // // add new property
+// person["gender"] = "male";
+// console.log(person);
+// //define cutom type property
+// let myVariable :any;
+// myVariable= 10
+// myVariable= "hello"
+// myVariable= true
+// Assignment # 21
+let items = {
+    "Dress": {
+        BrandName: "Sana Safinaz",
+        Price: 7000,
+        Size: "Medium"
+    },
+    "Mobile": {
+        Make: "Vivo",
+        Price: 15000,
+        Model: "Y90"
+    },
+    "Book": {
+        Subject: "Math",
+        Price: 530,
+        Level: "6th"
+    }
+};
+console.log(items);
---
+++ Step 21/app.ts
@@ -0,0 +1,26 @@
+
+
+// Assignment # 21
+
+/**They think of something you could store in a TypeScript Object. 
+ * Write a program that creates Objects containing these items */
+
+let items : {[key:string]:any} ={
+"Dress":{
+    BrandName: "Sana Safinaz",
+    Price: 7000,
+    Size: "Medium"
+},
+"Mobile":{
+    Make:"Vivo",
+    Price:15000,
+    Model:"Y90"
+
+},
+"Book":{
+    Subject:"Math",
+    Price:  530,
+    Level:  "6th"
+}
+}
+console.log(items)
