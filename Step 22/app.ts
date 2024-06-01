84850a8679f07540206879ece8d991ea97340112
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 12:12:43 -0700

    Add files via upload

---
+++ Step 22/app.ts
@@ -0,0 +1,13 @@
+// Assignment # 22 
+
+/*Intentional Error : If you havn't recieved an array index error in your programs ye, try to make one happen.
+change an index in one of your programs to produce am index error. make sure you  correct the error before 
+closing the program*/
+
+let intentionalError:number []=[1,2,3,4,5,6,7,8,9,0];
+
+console.log(intentionalError[11]);  // Intentionally errored
+
+
+var array = ['babar','Taha',1,2,3];
+console.log(array[4]); //correct
