b36f0d3612a69c199bf71fe573502856f1f8ece0
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 10:51:27 -0700

    Add files via upload

---
+++ Step 08/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assignment # 8
+console.log(4 + 4);
+console.log(50 - 42);
+console.log(2 * 4);
+console.log(40 / 5);
---
+++ Step 08/app.ts
@@ -0,0 +1,10 @@
+//Assignment # 8
+
+/**You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line */
+
+console.log(4+4);
+console.log(50-42);
+console.log(2*4);
+console.log(40/5)
