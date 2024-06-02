d3ec561e1acfb8db0d1083da781268374ffc38f5
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 23:13:34 -0700

    Add files via upload

---
+++ Step 35/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment # 35
+let animal_names = ["Lion", "Tiger", "Leopard"];
+animal_names.forEach(animal_name => {
+    console.log(`A ${animal_name} would not make a great pet.`);
+});
+console.log(`Any of these animals is territorial and has a strong sense  of pride and dominance`);
---
+++ Step 35/app.ts
@@ -0,0 +1,18 @@
+// Assignment # 35
+
+/**Animals: Think of at least three different animals that have a common characteristic.
+ *  Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+ *  • Modify your program to print a statement about each animal, such as A dog would make a great pet. 
+ * • Add a line at the end of your program stating what these animals have in common. You could print a sentence 
+ * such as Any of these animals would make a great pet! */
+
+let animal_names:string[] = ["Lion","Tiger","Leopard"];
+
+
+    animal_names.forEach(animal_name =>{
+        
+    
+    console.log(`A ${animal_name} would not make a great pet.`);
+   } );
+
+console.log(`Any of these animals is territorial and has a strong sense  of pride and dominance`);
