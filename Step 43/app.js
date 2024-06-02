d372f0570e6578416990a1d7c392f1e926af7c16
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 23:56:10 -0700

    Add files via upload

---
+++ Step 43/app.js
@@ -0,0 +1,28 @@
+"use strict";
+//  Assignment # 43
+/*Unchanged Magicians: Start with your work from Exercise 41.
+Call the function make_great() with a copy of the array of magicians’ names.
+  Because the original array will be unchanged, return the new array and store it in a separate array.
+  Call show_magicians() with each array to show that you have one array of
+ the original names and one array with the Great added to each magician’s name*/
+let magicians = ["David Copperfield", "Dynamo", "Penn and Teller", "Derren Brown"];
+function show_magicians(magicians) {
+    for (const magician of magicians) {
+        console.log(magician);
+    }
+}
+// // // Call the magician names
+// show_magicians(magicians)
+// Function to make magicians great through for loop.It will modify
+function make_great(magician) {
+    return magicians.map(name => `The Great ${name}`);
+}
+// to call make_great function
+// make_great(magicians)
+// show_magicians(magicians)
+let copy_magicians = magicians.slice();
+let copy_great_magicians = make_great(copy_magicians);
+console.log("\n Original array:\t");
+show_magicians(magicians);
+console.log("\n Copied array:\t");
+show_magicians(copy_great_magicians);
