49c9c1f19a5f20df6e4e79bb228b0b01b1aba151
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 23:17:49 -0700

    Add files via upload

---
+++ Step 36/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assignment # 36
+function make_shirt(size, message) {
+    console.log(`Making a ${size} T-shirt with a message "${message}" on it.`);
+}
+make_shirt("Medium", "One Ummah,One Voice");
---
+++ Step 36/app.ts
@@ -0,0 +1,12 @@
+// Assignment # 36
+
+/**
+T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message
+ that should be printed on the shirt. The function should print a sentence summarizing the size
+  of the shirt and the message printed on it. Call the function */
+
+function make_shirt(size:string,message:string){
+    console.log(`Making a ${size} T-shirt with a message "${message}" on it.`);
+    
+}
+make_shirt("Medium","One Ummah,One Voice");
