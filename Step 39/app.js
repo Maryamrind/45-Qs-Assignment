0c0037ca3af7af559b2495497890c87a982da716
Maryamrind <162865221+Maryamrind@users.noreply.github.com>
2024-06-01 23:34:36 -0700

    Add files via upload

---
+++ Step 39/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Assignment # 39
+function city_country(city, country) {
+    // return `${city},${country}`
+    console.log(`"${city}","${country}"`);
+}
+city_country("Karachi", "Pakistan");
+let mycities = city_country("Karachi", "Pakistan");
+console.log(mycities);
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Dehli", "India"));
+console.log(city_country("Dhaka", "Bangladesh"));
